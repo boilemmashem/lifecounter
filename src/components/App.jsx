4d52fdf65e0936199a965b6408f6cd42b921c6e4
Mike Hart <michael-hart@live.com>
2022-05-12 09:03:48 +0100

    creating new version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
-import React, {useState} from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { Player } from './Player';
-import { Menu } from './Menu';
 
 export const SettingsContext = React.createContext();
 
@@ -9,37 +7,9 @@ export const SettingsContext = React.createContext();
 const StyledApp = styled.div`
 `
 export function App() {
-
-  // Pull in settingsObj from localStorage
-  // This contains the state values from previous session
-  const settingsObj = JSON.parse(localStorage.getItem('settingsObj'));
-
-  // Hook contexts for settings
-  // Pull in settings from localstorage as the initial value
-  const [lifeTotal, setLifeTotal] = useState(settingsObj.lifeTotal ? settingsObj.lifeTotal: 20);
-  const [playerCount, setPlayerCount] = useState(settingsObj.playerCount ? settingsObj.playerCount: 1);
-
-  const makePlayers = (numOfPlayers) => {
-    let playersArr = [];
-    for(let i = 1; i <= numOfPlayers; i++) {
-      playersArr.push(
-        <Player
-          lifeTotal={lifeTotal}
-          defaultPlayerName={`Player ${i}`}
-          key={`player${i}`}
-        />
-      )
-    }
-    return playersArr;
-  }
-
-
   return (
     <StyledApp>
-      <SettingsContext.Provider value={{lifeTotal, setLifeTotal, playerCount, setPlayerCount}}>
-        <Menu/>
-      </SettingsContext.Provider>
-      {makePlayers(playerCount)}
+      Hello, World!
     </StyledApp>
   );
 }
