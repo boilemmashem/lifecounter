15640fcd2711ad5dd9228a043524db4bd7d54203
Mike Hart <michael-hart@live.com>
2022-05-12 12:19:43 +0100

    newgamemodal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import {NewGameModal} from './NewGameModal'
 
+// Create a context to hold global settings
 export const SettingsContext = React.createContext();
 
 
 const StyledApp = styled.div`
 `
 export function App() {
+
+  // Fetch settings from previous session in localStorage
+  // const settingsObj = JSON.parse(localStorage.getItem('settingsObj'));
+
+  const [playerCount, setPlayerCount] = useState(1)
+  const [startingLifeTotal, setStartingLifeTotal] = useState(40)
+  const [newGameOpen, setNewGameOpen] = useState(true)
+
   return (
     <StyledApp>
-      Hello, World!
+      <SettingsContext.Provider value={{
+        playerCount, setPlayerCount, startingLifeTotal, setStartingLifeTotal, newGameOpen, setNewGameOpen
+      }}>
+        <NewGameModal />
+      </SettingsContext.Provider>
     </StyledApp>
   );
 }
---
+++ src/components/NewGameModal.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+import { SettingsContext } from './App'
+
+const StyledNewGame = styled.div`
+    position: fixed;
+    z-index: 10;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .newGameSettings {
+        background: white;
+        max-width: 90%;
+    }
+`
+export const NewGameModal = () => {
+    // Get app settings from global context
+    const settings = React.useContext(SettingsContext)
+    
+    const startNewGame = () => {
+        // Store the new game settings and begin game
+        const newGamePlayerCount = parseInt(document.getElementById("newGamePlayerCount").value);
+        const newGameStartingLifeTotal = parseInt(document.getElementById("newGameStartingLifeTotal").value);
+
+        settings.setPlayerCount(newGamePlayerCount);
+        settings.setStartingLifeTotal(newGameStartingLifeTotal);
+        settings.setNewGameOpen(false);
+
+    }
+
+    if(settings.newGameOpen) {
+        return (
+            <StyledNewGame>
+                <div className="newGameSettings">
+                    <h1>Life Counter</h1>
+                    <div>
+                        <label htmlFor="newGamePlayerCount">Players: </label>
+                        <select id="newGamePlayerCount" defaultValue={settings.playerCount}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="newGameStartingLifeTotal">Life Total: </label>
+                        <input id="newGameStartingLifeTotal" type="number" defaultValue={settings.startingLifeTotal}/>
+                    </div>
+                    <div>
+                        <button className='btnNewGameStart' onClick={() => startNewGame()}>Start Game</button>
+                    </div>
+                </div>
+            </StyledNewGame>
+        )
+    }
+
+}
